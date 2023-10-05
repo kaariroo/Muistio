afa62e54fbd8015d07d345bd8fcbe3bdeeb3dc68
kaariroo <118998511+kaariroo@users.noreply.github.com>
2023-10-05 14:11:23 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE Npcs (id SERIAL PRIMARY KEY, name TEXT, describtion TEXT, location_
 
 CREATE TABLE Locations (id SERIAL PRIMARY KEY, name TEXT, describtion TEXT);
 
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, usertype TEXT);
 
 CREATE TABLE Location_notes (id SERIAL PRIMARY KEY, note TEXT, user_id INTEGER REFERENCES users, location_id INTEGER REFERENCES Locations);
